74c65f45d8dc6d682be043e5096cd63865c3f700
Jack Floyd <jack.jackfloyd@gmail.com>
2025-05-11 12:52:27 +0100

    Started Mah Jongg

---
+++ Exercises from Back of Book/mahJonggHands.py
@@ -0,0 +1,46 @@
+# A game is played with a deck of tiles.
+# there are three suits (dots, bamboo, and wan) and each is ranked 1-9
+# There are honours too; four winds (NSEW), and three dragons (WRG)
+
+# Define a parent class of Tile, and two subclasses, SuitTile and HonorTile
+# SuitTile has a suit and rank, HonourTile has only a name
+
+class Tile(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return Tile.getName()
+
+    def __eq__(self, other):
+        self.getName() == other.getName()
+
+    def __ne__(self, other):
+        self.getName() != other.getName()
+
+    def getSuit(self):
+        return NotImplemented
+
+    def getRank(self):
+        return NotImplemented
+
+    def getName(self):
+        return self.name
+
+
+class SuitTile(Tile):
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def getSuit(self):
+        return self.suit
+
+    def getRank(self):
+        return self.rank
+
+    def getName(self):
+        name = self.suit + self.rank
+        return name
+
+    def __lt__(self, other):
